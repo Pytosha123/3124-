108d8cc4d4b14633f96fda29cba44bb6f4a607a9
Anna Tumanova <pytosha@gmail.com>
2022-09-05 14:30:06 +0300

    realize hwtask8

---
+++ seminar1/hwtask8/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая на вход принимает число (N), 
+//а на выходе показывает все чётные числа от 1 до N
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int currentValue = 2;
+
+while(currentValue <=  number)
+{
+    Console.Write(currentValue + " ");
+    currentValue += 2; 
+}
