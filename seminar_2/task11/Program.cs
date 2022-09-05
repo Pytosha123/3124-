f60bdd56ddcbcd0cbf1a0766ee3bf6e7fa99eeb0
Anna Tumanova <pytosha@gmail.com>
2022-09-05 15:40:20 +0300

    realize task11

---
+++ seminar_2/task11/Program.cs
@@ -0,0 +1,16 @@
+﻿//1. Напишите программу, которая выводит случайное трёхзначное число 
+//и удаляет вторую цифру этого числа.
+//456 -> 46
+//782 -> 72
+//918 -> 98
+
+Random rand = new Random ();
+int number = rand.Next(10,1000);
+Console.WriteLine($"Сгенерировать случайное число: {number}");
+int digit3 = number % 10;
+int digit1 = number / 100;
+//Console.WriteLine(digit1);
+//Console.WriteLine(digit3);
+
+
+System.Console.WriteLine("Новое число: " + digit1 +digit3);
