5f0c18bc14f074e0f3c6af02f6edecde7894c6d8
Anna Tumanova <pytosha@gmail.com>
2022-09-06 21:34:57 +0300

    realize hw_task10

---
+++ seminar_2/hw_task10/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+//и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+///782 -> 8
+//918 -> 1
+
+Console.WriteLine("Введите трехзначное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number > 99 && number < 1000) 
+{
+    int digit = number / 10;
+    int result = digit % 10;
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("Число не трёхзначное");
+}
+
+
+
+
+
+
