023104c08e0642d3c6ef7c5d4a8a28554028f640
Anna Tumanova <pytosha@gmail.com>
2022-09-08 21:33:03 +0300

    realize task20

---
+++ seminar3/task20/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 21: Напишите программу, которая принимает на вход координаты двух точек 
+//и находит расстояние между ними в 2D пространстве.
+//A (3,6); B (2,1) -> 5,09
+//A (7,-5); B (1,-1) -> 7,21
+//AB = √ (xb - xa)2 + (yb - ya)2.
+
+
+Console.WriteLine("Введите коортдинату точки A по х");
+int xa = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите коортдинату точки A по y");
+int ya = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите коортдинату точки B по х");
+int xb = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите коортдинату точки B по y");
+int yb = Convert.ToInt32(Console.ReadLine());
+
+double res = Math.Sqrt(Math.Pow(xb - xa, 2) + Math.Pow(yb - ya, 2));
+Console.WriteLine(res);
