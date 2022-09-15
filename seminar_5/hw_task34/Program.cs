df4d1483493a770cdc9903394f765b77a660c66b
Anna Tumanova <pytosha@gmail.com>
2022-09-15 20:56:39 +0300

    realize hw_task34

---
+++ seminar_5/hw_task34/Program.cs
@@ -0,0 +1,33 @@
+﻿//Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+
+Console.WriteLine("Введите размер массива:  ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+ArrayRandomNumbers(numbers);
+PrintArray(numbers);
+int count = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+if (numbers[i] % 2 == 0)
+count++;
+
+void ArrayRandomNumbers(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(100,1000);
+    }
+}
+void PrintArray(int[] numbers)
+{
+    Console.Write("[]");
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.Write("[]]");
+    Console.WriteLine();
+}
+Console.WriteLine($"всего {numbers.Length} чисел, {count} из них чётные");
