8f97684dc8de01906f857ae99607a25574e401de
Anna Tumanova <pytosha@gmail.com>
2022-09-20 12:32:40 +0300

    realize hw_task35

---
+++ seminar_5/hw_task35/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 35: Задайте одномерный массив из 123 случайных чисел в диапазоне [0, 150]. 
+//Найдите количество элементов массива, значения которых лежат в отрезке [10,99].
+
+
+
+int N=123;
+int[] arr=new int[N];
+Random random=new Random();
+for(int i=0;i<arr.Length;i++)
+arr[i]=random.Next(0,10);
+for(int i=0;i<arr.Length;i++)
+System.Console.Write($"{arr[i],4}");
+
+int count=0;
+for(var i=0; i<arr.Length;i++)
+{
+if(i>10 && i<99)
+count++;
+}
+System.Console.WriteLine();
+System.Console.WriteLine(count);
